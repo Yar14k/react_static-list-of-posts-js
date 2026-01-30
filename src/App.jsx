cf5d44f8457478b11c6fbeed841ce17869ddbb6b
Yar14k <tabachuk1406@gmail.com>
2026-01-30 20:45:00 +0100

    Add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import './App.scss';
 
- import postsFromServer from './api/posts.json';
- import commentsFromServer from './api/comments.json';
- import usersFromServer from './api/users.json';
- import {PostList} from './components/PostList/PostList'
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList/PostList';
 
- const totalPosts = postsFromServer.map(post => ({
-    ...post,
-    user: usersFromServer.find(user => user.id === post.userId),
-    comments: commentsFromServer.filter(comment => comment.postId === post.id)
-  }));
+const totalPosts = postsFromServer.map(post => ({
+  ...post,
+  user: usersFromServer.find(user => user.id === post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+}));
 
 export const App = () => (
-
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <PostList posts={totalPosts}/>
+    <PostList posts={totalPosts} />
   </section>
 );
